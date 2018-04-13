80de39e9e6a0441981ca6819c017a38cd43f7301
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-13 21:07:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase(
+)
+}
+
+function whisper(string) {
+  return string.toLowerCase(
+)
+}
+
+function logShout(string) {
+  console.log("HELLO")
+  }
+logShout(string)
+
+function logWhisper(string) {
+  console.log("hello")
+}
+logWhisper(string)
+
+var lowercase = "string"
+sayHiToGrandma(string) 
+  if (lowercase.toLowerCase() === lowercase) {
+   console.log("I can't hear you!")
+  }
+sayHiToGrandma(string)
+
+
+
+
+
+
