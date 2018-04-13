7290e0ae8644d86f38e475415cb46322b2d66ed6
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-13 21:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ logWhisper(string)
 
 var lowercase= "hello"
 sayHitoGrandma(string)
-if (`${string}`.toLowerCase() === lowercase) {
+if (`${string} === string.toLowerCase) {
   console.log("I can't hear you!")
 }
   
